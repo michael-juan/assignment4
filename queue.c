8b56e8d30fce8667d28340a89e338b48b1e3aafb
Vineet Ramareddi <vramared@unix3.lt.ucsc.edu>
2017-05-31 22:01:46 -0700

    Added enqueue

--- queue.c
+++ queue.c
@@ -3,6 +3,14 @@
 # include <stdbool.h>
 # include <stdio.h>
 
+void swap(queue *q, uint32_t indexOne, uint32_t indexTwo)
+{
+	uint32_t temp = q -> Q[indexOne];
+        q -> Q[indexOne] = q -> Q[indexTwo];
+        q -> Q[indexTwo] = temp;
+	return;
+}
+
 queue *newQueue(uint32_t qSize)
 {
 	queue *q = (queue *) malloc(sizeof(queue));
@@ -37,22 +45,47 @@ bool full(queue *q)
 	}
 	return false;
 }
-/*
+
 bool dequeue(queue *q, item *i)
 {
-		
+	if (empty(q))
+	{
+		return false; 
+	}	
+	*i = q -> Q[q -> tail];
+	(q -> tail)++;
+	return true;
 }
-*/
+
 bool enqueue(queue *q, item i)
 {
+	if (full(q))
+	{
+		return false;
+	}
 	q -> Q[q -> head] = i;
+	uint32_t tracker = (q -> head);
+
+	if (q -> head > 0)
+	{
+		for (uint32_t i = tracker; i > (q -> tail); i--)
+		{	
+			if (q -> Q[tracker] < q -> Q[i-1])
+			{
+				swap(q, i-1, tracker);
+                		tracker = i-1;
+			}
+		}
+	}
+
 	(q -> head)++;
 	return true;
+
 }
 
 void printQueue(queue *q)
 {
-	for (uint32_t i = 0; i < q -> size; i++)
+	for (uint32_t i = 0; i < q -> head; i++)
 	{
 		printf("%u\t", q -> Q[i]);
 	}
@@ -62,13 +95,21 @@ void printQueue(queue *q)
 
 int main(void)
 {
-	printf("hi");
-	queue *q = newQueue(5);
+	queue *q = newQueue(10);
+	uint32_t *a = 500;
 	enqueue(q, 3);
-	enqueue(q, 4);
 	enqueue(q, 5);
+	enqueue(q, 4);
+	enqueue(q, 8);
+	enqueue(q, 2);
+	dequeue(q, a);
+	enqueue(q, 1);
+	enqueue(q, 50);
+	enqueue(q, 49);
+	enqueue(q, 9);
 	enqueue(q, 6);
 	printQueue(q);
+	delQueue(q);
 	return 0;
 }
 
