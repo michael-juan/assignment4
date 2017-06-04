aa162f511acfff3f6ff85871740e722af2f8d3f8
michael <michael@michaeljuan.co>
2017-06-03 17:11:44 -0700

    changed makefile to do both encode and decode and added flags to encode.c

--- encode.c
+++ encode.c
@@ -1,6 +1,7 @@
 # include <stdint.h>
 # include <stdio.h>
-#include <unistd.h>
+# include <unistd.h>
+# include <fcntl.h>
 # include "huffman.h"
 
 int main(void)
@@ -9,15 +10,42 @@ int main(void)
 	static uint32_t histogram[256];
 	histogram[0] = 0x01;
 	histogram[255] = 0x01;
+	uint64_t numBytes = 0;
 	uint8_t temp;
+	
 	while (fscanf(fp, "%c", &temp) == 1)
 	{
 		histogram[temp]++;
+		numBytes++;
 	}
+	
+	int file = open("fuckingfuck",O_CREAT | O_TRUNC | O_WRONLY,0644);
+	write(file, "\xAD\xDE\x0D\xD0", 4);
+	write(file, &numBytes, sizeof(uint64_t));
+	
+	close(file);
 	fclose(fp);
-	for(int i = 0; i < 256; i++)
-	{
-		printf("%x %u \n",i,histogram[i]);
-	}
+// 	for(int i = 0; i < 256; i++)
+// 	{
+// 		printf("%x %u \n",i,histogram[i]);
+// 	}
 	return 0;
 }
+//magic number number of bytes size of tree
+//while bytes written < total bytes continue looping
+//deadD00d total bytes size of tree tree encoded file trailing zeros
+
+//step 1 histogram thing you see most
+//priority queue make nodes
+
+//histogram pop pop join push pop pop join push
+//priority queue based on count
+//professor did preorder traversal
+//postorder left right nodes
+
+//do full postorder traversal
+// put into stack
+// push push push unti its leaf nodes
+// set the table to c0de
+// move through huffman tree post order using stack and then 
+// code table ['s'] = stack;
