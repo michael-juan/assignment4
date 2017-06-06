119418bf9b2edd2153ab9fd44eb1d996f7bf9b85
michael <michael@michaeljuan.co>
2017-06-05 20:15:11 -0700

    testing code

--- encode.c
+++ encode.c
@@ -4,10 +4,9 @@
 # include <fcntl.h>
 # include "huffman.h"
 # include "queue.h"
-
+# include "bv.h"
 int main(void)
 {
-	//FILE *fp = fopen("badspeak.txt", "r");
 	int inputFile = open("badspeak.txt",O_RDONLY);
 	static uint32_t histogram[256];
 	histogram[0] = 0x01;
@@ -30,10 +29,9 @@ int main(void)
 			enqueue(histogramQueue, newNode(i, 1, histogram[i]));
 		}
 	}
-	printQueue(histogramQueue);
 	treeNode *itemA = newNode('$', 0, 0);
 	treeNode *itemB = newNode('$', 0, 0);
-
+	
 	while(empty(histogramQueue) == 0 )
 	{
 		dequeue(histogramQueue, &itemA);
@@ -45,31 +43,49 @@ int main(void)
 		enqueue(histogramQueue,join(itemA, itemB));
 	
 	}
-	printTree(itemA,10);
+//	printTree(itemA,10);
 	code s = newCode();
 	code codeTable[256];
 	buildCode(itemA, s, codeTable);
-
-// 	for(int i = 0; i < numBytes; i++ )
-// 	{
-// 		
-// 	}
+	printCode(&codeTable[122]);
+	printTree(itemA,1);
+	//uint8_t inputFile
+	//uint8_t buffer[8192];
+	bitV *test = newVec(5);
+	code t = newCode();
+	t.bits = {0,0,1};
+	t.l = 3;
+	test->l = t.l+test->l;
+	test ->v =realloc((test->l/8)+1, sizeof(uint8_t));
+	for(uint32_t i = 0; i < numBytes; i++ )
+	{
+		
+	}
 	
 	
 	
-	int file = open("fuckingfuck",O_CREAT | O_TRUNC | O_WRONLY,0644);
+	int file = open("testoutput",O_CREAT | O_TRUNC | O_WRONLY,0644);
 	write(file, "\xAD\xDE\x0D\xD0", 4);
 	write(file, &numBytes, sizeof(uint64_t));
 	
 	close(inputFile);
 	close(file);
-//	fclose(fp);
-// 	for(int i = 0; i < 256; i++)
-// 	{
-// 		printf("%x %u \n",i,histogram[i]);
-// 	}
 	return 0;
 }
+// void shiftl(void *object, size_t size)
+// {
+//    unsigned char *byte;
+//    for ( byte = object; size--; ++byte )
+//    {
+//       unsigned char bit = 0;
+//       if ( size )
+//       {
+//          bit = byte[1] & (1 << (CHAR_BIT - 1)) ? 1 : 0;
+//       }
+//       *byte <<= 1;
+//       *byte  |= bit;
+//    }
+// }
 //magic number number of bytes size of tree
 //while bytes written < total bytes continue looping
 //deadD00d total bytes size of tree tree encoded file trailing zeros
