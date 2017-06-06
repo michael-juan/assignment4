109e53d659b7b6dc68c6a66a2bdc27ba54e9b3a4
Vineet Ramareddi <vramared@unix2.lt.ucsc.edu>
2017-06-05 19:56:20 -0700

    progress in decode

--- decode.c
+++ decode.c
@@ -5,44 +5,80 @@
 # include <stdint.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdbool.h>
+# include <ctype.h>
 # include "bv.h"
 
-/* DONT FORGET TO CHECK THE MAGIC NUMBER!!!!*/
 
-int main(void)
+int main(int argc, char **argv)
 {
-	uint32_t magicNum = 0;
-	uint16_t treeSize = 0;
-	uint64_t fileSize = 0;
-	int fd = open("secret.zzZ", O_RDONLY);
-	read(fd, &magicNum, 4);
-	read(fd, &fileSize, 8);
-	read(fd, &treeSize, 2);
+	int c, fd = 0;
+	bool verbose = false, showTree = false;
 
-//	printf("Magic No: %X\n", magicNum);
+	while ((c = getopt(argc, argv, "i:o:vp")) != -1)
+	{
+		switch (c)
+		{
+			case 'i':
+			{
+				fd = open(optarg, O_RDONLY);
+				if (fd == 0)
+				{
+					printf("Please specify an input file");
+					return 0;
+				}
+				break;
+			}
+			case 'v':
+			{
+				verbose = true;
+				break;
+			}
+			case 'p':
+			{
+				showTree = true;
+				break;
+			}
+			default:
+			{
+				break;
+			}
+			
+		}
+	} 
+
+	
+	uint32_t magicNum = 0;
+
+	read(fd, &magicNum, 4);
 	
 	if (magicNum != 0xdeadd00d)
 	{
 		printf("Invalid File: Magic Number does not match.\n");
 		return 0;
 	}	
-//	printf("File Size: %lu\n", fileSize);
 	
-//	printf("Tree Size: %d\n", treeSize);
+	uint16_t treeSize = 0;
+        uint64_t fileSize = 0;
+
+	read(fd, &fileSize, 8);
+        read(fd, &treeSize, 2);
 	
 	uint8_t *savedTree = malloc(sizeof(uint8_t)*treeSize);	
 	read(fd, savedTree, treeSize);
 	
 	treeNode *tree = loadTree(savedTree, treeSize);
-//	printTree(tree, 1);
 			
+	treeNode *t = tree;
+	
+	uint8_t *buf = malloc(sizeof(uint8_t)*fileSize);
 	uint64_t bytesDecoded = 0;
 	
-	treeNode *t = tree;
 	while (bytesDecoded < fileSize)
 	{
-		bitV *bits = newVec(8);
-		read(fd, bits->v, 1);
+		bitV *bits = newVec(4096);
+		read(fd, bits->v, 512);
+		
 		for (uint32_t i = 0; i < bits->l; i++)
                 {
                         if (valBit(bits, i) == 0)
@@ -55,19 +91,31 @@ int main(void)
 			}
 			if (t -> leaf)
 			{
-				write(1, &t -> symbol, 1);
+				buf[bytesDecoded] = t -> symbol;
 				t = tree;
 				bytesDecoded++;
 			}
-			if(bytesDecoded == fileSize)
+			if (bytesDecoded == fileSize)
 			{
 				break;
 			}
                 }
 		delVec(bits);
 	}
+	
+	if (verbose)
+	{
+		printf("Original %lu bits: tree (%u)\n", fileSize*8, treeSize);
+	}
+	
+	write(1, buf, bytesDecoded);
+	
+	if (showTree)
+	{
+		printTree(tree, 1);
+	} 
 
-
+	free(buf);
 	close(fd);
 	free(savedTree);
 	delTree(tree);
